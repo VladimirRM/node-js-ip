eb0795c6035a142c082fa3c0761d45e4bedda483
Vladimir RM <shvdrummer@gmail.com>
2023-04-27 17:34:29 +0300

    rm exercise

---
+++ index.js
@@ -0,0 +1,7 @@
+const CC  = require('currency-converter-lt')
+
+let currencyConverter = new CC({from:"USD", to:"EUR", amount:100})
+
+currencyConverter.convert().then((response) => {
+    console.log(`Result:${response}`) //or do something else
+})
