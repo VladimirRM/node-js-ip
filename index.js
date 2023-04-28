83889a654334c52ca08f4c49404b3ab5e7513aac
Vladimir RM <shvdrummer@gmail.com>
2023-04-28 08:48:24 +0300

    Rm folder & file

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const fs = require('fs')
 
 // fs.mkdirSync('Text-file')
 
-fs.mkdir('Text-files',()=>{
-    fs.writeFile('./Text-files/Some-text.txt','Hello',()=>{
+// fs.mkdir('Text-files',()=>{
+//     fs.writeFile('./Text-files/Some-text.txt','Hello',()=>{
+
+//     })
+// })
+
+fs.unlink('./Text-files/Some-text.txt',()=>{
+    fs.rmdir('./Text-files',()=>{
         
     })
 })
