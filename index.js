3793533b27d8012f61971fc763010009bf1aba69
Vladimir RM <shvdrummer@gmail.com>
2023-04-28 09:37:59 +0300

    Add new p

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const http = require('http')
 
 let server = http.createServer((req,res)=>{
    res.writeHead(200,{'Content-type':'text/html; charset=utf-8'})
-   res.end('Hello <b> node js</b>')
+   res.end(`<!DOCTYPE html>
+   <html lang="en">
+   <head>
+       <meta charset="UTF-8">
+       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>Node JS</title>
+   </head>
+   <body>
+       <h1>Hello node</h1>
+   </body>
+   </html>`)
 })
 
 
